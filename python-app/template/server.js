f8cfabbe672619e18c718a9b78fc3a26d9074169
Suresh <sureshmavp1@gmail.com>
2026-02-20 09:04:47 +0000

    Loadbalance Port updated and Display Message

--- python-app/template/server.js
+++ python-app/template/server.js
@@ -1,8 +1,23 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from my Docker container!\n");
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  res.end(`
+    <html>
+      <head>
+        <title>Backstage SSP</title>
+      </head>
+      <body style="background-color:#0f172a; text-align:center; padding-top:100px;">
+        <h1 style="color:#38bdf8; font-size:48px;">
+          🚀 Welcome to Backstage Self Service Portal!
+        </h1>
+        <p style="color:white; font-size:20px;">
+          Running successfully inside Docker Container
+        </p>
+      </body>
+    </html>
+  `);
 });
 
 const PORT = 3000;
