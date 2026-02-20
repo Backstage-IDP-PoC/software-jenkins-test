cf7ceb735864e75909efbb7f51252829fbf6f372
Suresh <sureshmavp1@gmail.com>
2026-02-20 12:49:05 +0000

    Latest Update

--- python-app/template/server.js
+++ python-app/template/server.js
@@ -1,27 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-
-  res.end(`
-    <html>
-      <head>
-        <title>Backstage SSP</title>
-      </head>
-      <body style="background-color:#0f172a; text-align:center; padding-top:100px;">
-        <h1 style="color:#38bdf8; font-size:48px;">
-          🚀 Welcome to Backstage Self Service Portal!
-        </h1>
-        <p style="color:white; font-size:20px;">
-          Running successfully inside Docker Container
-        </p>
-      </body>
-    </html>
-  `);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello! Welcome to Self-Service Portal!\n");
 });
 
 const PORT = 3000;
 
-server.listen(PORT, "0.0.0.0", () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
